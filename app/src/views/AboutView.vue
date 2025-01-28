57ef148dd89e5234b62f283cb376ddbd0bfa822e
Selina227 <selinac33@nycstudents.net>
2025-01-28 12:43:50 -0500

    e

---
+++ app/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NewList />
+    <form name="login-form" @submit.prevent="login(user)">
+      <label for="username">Enter Username</label>
+      <input type="text" v-model="user.username" />
+      <label for="password">Enter Password</label>
+      <input type="text" v-model="user.password" />
+      <button class="submit-button" type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import NewList from '../components/NewList.vue'
+import { reactive } from 'vue'
+const user = reactive({ username: '', password: '' })
+function login(user) {
+  console.log(user)
+}
+</script>
+
+<style scoped></style>
---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="div">
+    <h1>{{ count }}</h1>
+    <button @click="increment">Click Me</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+let count = ref(0)
+function increment() {
+  count.value++
+}
+</script>
+
+<style scoped></style>
