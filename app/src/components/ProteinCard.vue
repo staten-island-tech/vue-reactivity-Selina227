259cdececdbeba18d6a8749404a92b965b6ad602
Selina227 <selinac33@nycstudents.net>
2025-02-03 12:30:18 -0500

    e

---
+++ app/src/components/ProteinCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <h2>{{ topping.name }}</h2>
+    <img :src="topping.image" alt="" />
+    <button @click="add"></button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  topping: Object,
+})
+</script>
+
+<style scoped></style>
--- app/src/components/ToppingCard.vue
+++ app/src/components/ToppingCard.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="card">
     <h2>{{ topping.name }}</h2>
     <img :src="topping.image" alt="" />
+    <button @click="add"></button>
   </div>
 </template>
 
